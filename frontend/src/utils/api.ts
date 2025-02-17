f70300c7f1dc15cdbbd86c09af156b8fc3e99cfc
Highzahawk <azhan2000@outlook.com>
2025-02-16 22:30:43 -0800

    Front end start
    started front end stuff, connecting backend to frontend

---
+++ frontend/src/utils/api.ts
@@ -0,0 +1,19 @@
+// src/utils/api.ts
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:5000/api";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const setAuthToken = (token: string | null) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
